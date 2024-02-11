8673aea5708aaa662110340648530aeaddb5cf25
vorogurcov <vania.frol2010@yandex.ru>
2024-02-11 21:14:39 +0300

    Slightly debugged header.h. Implemented GetSolution func and some static additional funcs.

--- SquareEquationSolver/Header.h
+++ SquareEquationSolver/Header.h
@@ -1,16 +1,20 @@
 #pragma once
 #pragma warning(disable:4996)
 #include <stdio.h>
-
+#include <math.h>
+#include <stdlib.h>
 
 struct Equation
 {
 	double a, b, c;
 } typedef Equation;
 
+enum Status
+{
+	NoSol = 0, OneSol = 1, TwoSol = 2
+};
 struct Solution
 {
-	enum Status
-		NoSol, OneSol, TwoSol;
+	enum Status Cur;
 	double* x1,* x2;
 } typedef Solution;
--- SquareEquationSolver/Main.c
+++ SquareEquationSolver/Main.c
@@ -1,13 +1,14 @@
 #include "Header.h"
 
 extern void User_Interface(Equation const* OurEquation);
+extern void GetSolution(Equation const* OurEquation, Solution* const OurSolution);
 
 int main(int argc, char* argv)
 {
 	Equation OurEquation;
-
+	Solution OurSolution;
 	User_Interface(&OurEquation);
-
+	GetSolution(&OurEquation, &OurSolution);
 
 	return 0;
 }
